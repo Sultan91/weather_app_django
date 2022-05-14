3aef5426840441112aabd9813cd9f8e062e82c06
Sultan91 <erumbaevs@mail.ru>
2022-05-14 11:25:02 +0600

    setup for heroku

--- the_weather/the_weather/settings.py
+++ the_weather/the_weather/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_%8&aaez4#5%6)$$#rxdbyv5)(he7(2&@_d77_00tehjkrj$n2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
--- the_weather/weather/views.py
+++ the_weather/weather/views.py
@@ -3,8 +3,9 @@ from django.shortcuts import render
 from .models import City
 from .forms import CityForm
 
+
 def index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_API_KEY'
+    url = 'http://api.openweathermap.org/data/2.5/weather?id=524901&appid=274a26ce9ee87a14a4fc95d8d0cd2ffe&'
 
     if request.method == 'POST':
         form = CityForm(request.POST)
@@ -17,17 +18,16 @@ def index(request):
     weather_data = []
 
     for city in cities:
-
         r = requests.get(url.format(city)).json()
-
+        print(r)
         city_weather = {
-            'city' : city.name,
-            'temperature' : r['main']['temp'],
-            'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon'],
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
         }
 
         weather_data.append(city_weather)
 
-    context = {'weather_data' : weather_data, 'form' : form}
+    context = {'weather_data': weather_data, 'form': form}
     return render(request, 'weather/weather.html', context)
